a8cd6c3f6a154641b7c82a8cc58467e5435c4c16
Luca Gaddini <lucagaddini@icloud.com>
2023-04-01 15:48:14 +0200

    Replaced flex items-end with flex items-center

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -27,8 +27,8 @@ const Sidebar = () => {
   ]
 
   return ( 
-    <div className="col-span-1 h-full pr-4 md:pr-6">
-      <div className="flex flex-col items-end">
+    <div className="col-span-1 h-full p-4 md:pr-6">
+      <div className="flex flex-col items-center">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
           {items.map((item) => (
