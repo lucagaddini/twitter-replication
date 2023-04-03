6b7624f53039697a78a6f87d3612c281c01985a1
Luca Gaddini <luca.gaddini@zucchetti.it>
2023-04-03 09:55:52 +0200

    Added LoginModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return( 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+  return(
+    <>
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
