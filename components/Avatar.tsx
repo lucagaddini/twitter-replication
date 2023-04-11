7140870697af9746f45b02264ef0ef68587cd103
lucagaddini <lucagaddini@icloud.com>
2023-04-11 18:48:05 +0200

    Compleated: FollowerBar

---
+++ components/Avatar.tsx
@@ -0,0 +1,50 @@
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useCallback } from "react";
+
+interface AvatarProps {
+  userId: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  const { data: fetchedUser } = useUser(userId);
+  const router = useRouter();
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      const url = `/users/${userId}`;
+
+      router.push(url);
+    },
+    [router, userId]
+  );
+
+  return (
+    <div
+      className={`
+      ${isLarge ? "h-32" : "h-12"}
+      ${isLarge ? "w-32" : "w-12"}
+      ${hasBorder ? "border-4 border-black" : ""}
+      rounded-full
+      hover:opacity-90
+      transition
+      cursor-pointer
+      relative
+    `}
+    >
+      <Image
+        fill
+        style={{ objectFit: "cover", borderRadius: "100%" }}
+        alt="Avatar"
+        onClick={onClick}
+        src={fetchedUser?.profileImage || "/images/placeholder.png"}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
